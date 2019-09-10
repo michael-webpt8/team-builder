7e1218ab835062a01d2f82e2a5a032cecc5440d6
Michael Williams <manicdesigner@gmail.com>
2019-09-10 18:41:03 -0400

    add beginning state

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const[team, setTeam] = useState([
+    {
+      name: 'Mikey',
+      email: 'mikeyjhavoc@gmail.com',
+      role: 'Backend Dev'
+    },
+    {
+      name: 'Marc',
+      email: 'Markymarc@gmail.com',
+      role: 'Database Admin',
+    },
+    {
+      name: 'Jacob',
+      email: 'cupcakeJake@gmail.com',
+      role: 'Front End Dev'
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
