9a6a50ec9e2bd5a70cf79bc9a3d4073aeb6838ea
Michael Williams <manicdesigner@gmail.com>
2019-09-10 20:12:18 -0400

    add handleChange, handleSubmit resetForm and rest of form

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,16 +1,46 @@
 import React, {useState}  from 'react';
 
 function Form(props) {
-    const initialTeammate = { user: '', email: '', role: ''};
-    const[newTeammate, setNewTammate] = useState(initialTeammate);
+    const { setTeammates, teammates } = props;
+    const initialTeammate = { name: '', email: '', role: ''};
+    const[newTeammate, setNewTeammate] = useState(initialTeammate);
+
+
+    const handleChange = event => {
+      setNewTeammate({...newTeammate, [event.target.name]: event.target.value});
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        props.setTeammates([newTeammate, ...props.teammates]);
+        console.log(teammates);
+        resetForm();
+    }
+
+    const resetForm = () => {
+        setNewTeammate(initialTeammate);
+    }
+
+  
     return (
         <div>
-            <form>
-                <label htmlFor="user-name">Name:</label>
-                    <input type="text" id="user-name" />
-               
+          <form onSubmit={handleSubmit}> 
+            <label htmlFor="user-name">Name:</label>
+                <input type="text" id="user-name" name="name" onChange={handleChange} value={newTeammate.name} />
+                <br />
+            <label htmlFor="user-email">Email:</label>
+                <input type="email" id="user-email" name="email" onChange={handleChange} value={newTeammate.email} />
+                <br />
+            <label htmlFor="user-role">Role:</label>
+                <input type="text" id="user-role" name="role" onChange={handleChange} value={newTeammate.role}  />
+                <br />
+            <button type="submit">Add User</button>
+            <button type="button" onClick={resetForm}>Reset</button>
+            
+
                  
-            </form>
+          </form>
         </div>
     );
 }
